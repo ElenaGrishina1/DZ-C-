1a767a361ddba9f91c990144ad0c002e5e3447bb
SacredAngel <sacredange7@gmail.com>
2023-01-14 14:13:28 +0300

    Домашка семинар 1

---
+++ DZ1/4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+ System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+if (number == 1)
+{
+    Console.WriteLine ("Нет четных чисел");
+}
+else
+{
+    Console.WriteLine ($"Все четные числа до {number}");
+while (count < (number -1))
+    {
+    count = count +2;
+    Console.WriteLine (count);
+    }
+}
