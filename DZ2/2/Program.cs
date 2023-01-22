2c012aa6045b1d2555f65b442d1e1785859ced9d
SacredAngel <sacredange7@gmail.com>
2023-01-22 16:53:36 +0300

    дз 2 семинар

---
+++ DZ2/2/Program.cs
@@ -0,0 +1,28 @@
+﻿// / Напишите программу, которая выводит третью цифру заданного числа или сообщает,  
+// что третьей цифры нет. 
+// 645 -> 5 
+// 78 -> третьей цифры нет 
+// 32679 -> 6
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num < 1000) 
+{
+    num = (num % 10);
+    Console.WriteLine($"Третья цифра: {num}");
+}
+else if (num > 999)
+{
+    while (num >= 999)
+    {
+        num = num / 10;
+    }
+    num = (num % 10);
+    Console.WriteLine($"Третья цифра: {num}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
